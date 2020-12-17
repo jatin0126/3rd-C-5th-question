cb762a8b06ca60231f25858ad397357cf804f12e
jatin0126 <75829245+jatin0126@users.noreply.github.com>
2020-12-17 13:14:58 +0530

    Update debug_exam.py

--- debug_exam.py
+++ debug_exam.py
@@ -1,4 +1,3 @@
-'''
 This function takes two arguments,
 data1 and data2, which contain
 key-value pairs. All key-value
@@ -10,7 +9,6 @@ in data1 and (k, v2) in data2 with a
 common key k. In this case, v1 and
 v2 may be the same, or v1 and v2 may
 be different.
-
 This function should modify only
 data1 and return a (possibly empty)
 dictionary as follows:
@@ -25,7 +23,6 @@ should be removed from data1 and the
 key-value pair (k, [v1, v2]) should
 be added to the (initially empty)
 dictionary to be returned.
-
 In this implementation, data1 is a
 dictionary and data2 is a list where
 each key-value pair in data2 is also
@@ -37,36 +34,43 @@ def uniqueUpdate(data1, data2):
     dupKeys = {}
 
     # Examine every (k, v2) pair in data2
-    for [k, v2] in data2:
-        # Check if there is a key-value
-        # pair with key = k in data1
-        if k in data1:
-            v1 = data1[k]
-            # (k, v1) in dict1
-            # Check if v1 != v2
-            if v1 != v2:
-                # Add (k, [v1, v2])
-                # to dictionary                
-                dupKeys[k] = [v1, v2]
-                # Remove (k, v1) from data1
-                del data1[k]
-            else:
-                # Add (k, v2) to data1
-                data1[k] = v2
+    for k, v2 in data2.items():
+        # Search for a key-value pair
+        # with key = k in data1
+        # (no such pair found yet)
+        kFound = False
+
+        for [k1, v1] in data1:
+            if k1 == k:
+                # Found pair with key = k
+                kFound = True
+
+                if v1 != v2:
+                	# Remove (k, v1) from data1
+                	data1.remove([k,v1])
+                	# Add (k, [v1, v2])
+                	# to dictionary
+                	dupKeys[k] = [v1,v2]
+  
+        # After the loop, check if
+        # k was not found
+        if not kFound:
+            # Add (k, v2) to data1
+            data1.append([k, v2])
+
     # After processing all (k, v2) in
     # data2, return the dictionary
     return dupKeys
 
 '''
 Visualize this function on an example:
-https://tinyurl.com/...
+https://tinyurl.com/dsaprac20
 '''
 
 ## DO NOT MODIFY BELOW THIS LINE! ##
 '''
 This part of the code reads input in
 the following format:
-
 Line 1: A positive integer n1
 representing the number of key value
 pairs in data1
@@ -81,33 +85,31 @@ Lines n1+3 to n1+n2+2: Two integers
 k and v per line representing the
 key and value (these n2 key value
 pairs are added to data2)
-
 This also prints the output in the
 following format after calling the
 uniqueUpdate function:
-
 data1
 data2 (should remain the same)
 dup (the dictionary returned)
 '''
 
 import sys
-if __name__ == '__main__':
-    data1 = {}
+if _name_ == '_main_':
+    data1 = []
     n1 = int(input())
     for _ in range(n1):
         k, v = map(int, input().split())
-        if k in data1:
-            sys.exit("Illegal: data1")
-        data1[k] = v
-    data2 = []
+        for [k1, v1] in data1:
+            if k1 == k:
+                sys.exit("Illegal: data1")
+        data1.append([k, v])
+    data2 = {}
     n2 = int(input())
     for _ in range(n2):
         k, v = map(int, input().split())
-        for [k2, v2] in data2:
-            if k2 == k:
-                sys.exit("Illegal: data2")
-        data2.append([k, v])
+        if k in data2:
+            sys.exit("Illegal: data2")            
+        data2[k] = v
     dup = uniqueUpdate(data1, data2)
     print(data1)
     print(data2)
